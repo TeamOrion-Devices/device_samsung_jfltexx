6f304822644d8b71322bd1709159933dfda4a30b
minidude2012 <chrissnmitchell@outlook.com>
2015-11-30 22:27:15 +0000

    Orionify

--- cm.mk
+++ cm.mk
@@ -1,10 +1,10 @@
 $(call inherit-product, device/samsung/jfltexx/full_jfltexx.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/orion/config/nfc_enhanced.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common SLIM stuff.
+$(call inherit-product, vendor/orion/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
   PRODUCT_NAME=jfltexx \
@@ -12,6 +12,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
   BUILD_FINGERPRINT="samsung/jfltexx/jflte:5.0.1/LRX22C/I9505XXUHOB7:user/release-keys" \
   PRIVATE_BUILD_DESC="jfltexx-user 5.0.1 LRX22C I9505XXUHOB7 release-keys"
 
+PRODUCT_NAME := orion_jfltexx
 PRODUCT_DEVICE := jfltexx
-PRODUCT_NAME := cm_jfltexx
 
